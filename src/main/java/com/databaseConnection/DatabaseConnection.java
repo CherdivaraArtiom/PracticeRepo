639ba46301f0d8fff3b2826cdf14ffebbd40786c
Cherdivara Artiom <artiom.cherdivara@gmail.com>
2019-03-04 23:41:34 +0200

    Add method that will return all elements in database

--- src/main/java/com/databaseConnection/DatabaseConnection.java
+++ src/main/java/com/databaseConnection/DatabaseConnection.java
@@ -1,7 +1,15 @@
 package com.databaseConnection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseConnection {
 
+    public DatabaseConnection () { }
 
-    
+
+    public List<String> getAllElements(){
+        List<String> allElements= new ArrayList<String>();
+        return allElements;
+    }
 }
